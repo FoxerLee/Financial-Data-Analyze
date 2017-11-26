fd5e15316c67ca97f9084f62a1657f2199e73a81
FoxerLee <foxerlee1@gmail.com>
2017-11-26 23:54:49 +0800

    Historical data ETL

--- SimpleData/JavaEE建表.sql
+++ SimpleData/JavaEE建表.sql
@@ -46,19 +46,19 @@ CREATE TABLE data_months(
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE data_years(
-	id INT NOT NULL AUTO_INCREMENT,
-	code VARCHAR(6) NOT NULL,
-	trading_day DATETIME NOT NULL,
-	open_value DOUBLE NOT NULL,
-	close_value DOUBLE NOT NULL,
-	high_value DOUBLE NOT NULL,
-	low_value DOUBLE NOT NULL,
-	volume_value DOUBLE NOT NULL,
-	PRIMARY KEY (id)
-);
+-- CREATE TABLE data_years(
+-- 	id INT NOT NULL AUTO_INCREMENT,
+-- 	code VARCHAR(6) NOT NULL,
+-- 	trading_day DATETIME NOT NULL,
+-- 	open_value DOUBLE NOT NULL,
+-- 	close_value DOUBLE NOT NULL,
+-- 	high_value DOUBLE NOT NULL,
+-- 	low_value DOUBLE NOT NULL,
+-- 	volume_value DOUBLE NOT NULL,
+-- 	PRIMARY KEY (id)
+-- );
 
-# warehouse
+-- warehouse
 CREATE TABLE warehouse_data_real_time(
 	id INT NOT NULL AUTO_INCREMENT,
 	code VARCHAR(6) NOT NULL,
@@ -116,27 +116,26 @@ CREATE TABLE warehouse_data_months(
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE warehouse_data_years(
-	id INT NOT NULL AUTO_INCREMENT,
-	code VARCHAR(6) NOT NULL,
-	trading_day DATETIME NOT NULL,
-	open_value DOUBLE NOT NULL,
-	close_value DOUBLE NOT NULL,
-	high_value DOUBLE NOT NULL,
-	low_value DOUBLE NOT NULL,
-	volume_value DOUBLE NOT NULL,
-	ma5 DOUBLE NOT NULL,
-	ma10 DOUBLE NOT NULL,
-	ma20 DOUBLE NOT NULL,
-	PRIMARY KEY (id)
-);
+-- CREATE TABLE warehouse_data_years(
+-- 	id INT NOT NULL AUTO_INCREMENT,
+-- 	code VARCHAR(6) NOT NULL,
+-- 	trading_day DATETIME NOT NULL,
+-- 	open_value DOUBLE NOT NULL,
+-- 	close_value DOUBLE NOT NULL,
+-- 	high_value DOUBLE NOT NULL,
+-- 	low_value DOUBLE NOT NULL,
+-- 	volume_value DOUBLE NOT NULL,
+-- 	ma5 DOUBLE NOT NULL,
+-- 	ma10 DOUBLE NOT NULL,
+-- 	ma20 DOUBLE NOT NULL,
+-- 	PRIMARY KEY (id)
+-- );
 
 CREATE TABLE company_info(
 	id INT NOT NULL AUTO_INCREMENT,
 	code VARCHAR(6) NULL,
 	name TEXT NULL,
 	lead_underwriter TEXT NULL,
-	profile TEXT NULL,
 	en_name TEXT NULL,	
 	total_name TEXT NULL,
 	listing_date TEXT NULL,
@@ -154,6 +153,85 @@ CREATE TABLE connect(
 	c_name VARCHAR(50) NULL,
 	PRIMARY KEY (code)
 );
+
+CREATE TABLE profile_data(
+	id INT NOT NULL AUTO_INCREMENT,
+	code VARCHAR(6) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	roe DOUBLE NULL,
+	net_profit_ratio DOUBLE NULL,
+	gross_profit_ratio DOUBLE NULL,
+	net_profits DOUBLE NULL,
+	eps DOUBLE NULL,
+	business_income DOUBLE NULL,
+	bips DOUBLE NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE operation_data(
+	id INT NOT NULL AUTO_INCREMENT,
+	code VARCHAR(6) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	arturnover DOUBLE NULL,
+	arturndays DOUBLE NULL,
+	inventory_turnover DOUBLE NULL,
+	inventory_days DOUBLE NULL,
+	currentasset_turnover DOUBLE NULL,
+	currentasset_days DOUBLE NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE growth_data(
+	id INT NOT NULL AUTO_INCREMENT,
+	code VARCHAR(6) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	mbrg DOUBLE NULL,
+	nprg DOUBLE NULL,
+	nav DOUBLE NULL,
+	targ DOUBLE NULL,
+	epsg DOUBLE NULL,
+	seg DOUBLE NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE debtpaying_data(
+	id INT NOT NULL AUTO_INCREMENT,
+	code VARCHAR(6) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	currentratio DOUBLE NULL,
+	quickratio DOUBLE NULL,
+	cashratio DOUBLE NULL,
+	icratio DOUBLE NULL,
+	sheqratio DOUBLE NULL,
+	adratio DOUBLE NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE cashflow_data(
+	id INT NOT NULL AUTO_INCREMENT,
+	code VARCHAR(6) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	cf_sales DOUBLE NULL,
+	rateofreturn DOUBLE NULL,
+	cf_nm DOUBLE NULL,
+	cf_liabilities DOUBLE NULL,
+	cashflowratio DOUBLE NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE user_info(
+	user_id INT NOT NULL AUTO_INCREMENT,
+	password VARCHAR(20) NOT NULL,
+	name VARCHAR(100) NOT NULL,	
+	PRIMARY KEY (user_id)
+);
+
+CREATE TABLE self_stocking(
+	user_id INT NOT NULL,
+	code VARCHAR(6) NOT NULL,
+	PRIMARY KEY (user_id, code)
+);				
+
 
 
 
