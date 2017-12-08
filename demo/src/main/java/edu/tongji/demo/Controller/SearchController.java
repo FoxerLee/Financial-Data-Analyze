e8e4e8dc024b316a10c464396dd576591a070c4e
hujiaxin <470348052@qq.com>
2017-12-08 21:47:59 +0800

    add something

--- demo/src/main/java/edu/tongji/demo/Controller/SearchController.java
+++ demo/src/main/java/edu/tongji/demo/Controller/SearchController.java
@@ -17,36 +17,37 @@ public class SearchController {
     @PostMapping("/Stocks")
     public Object SearchStock(@RequestBody String content){
 
-        //将post过来的数据转换为json格式
-        JSONObject jsonObject;
-        try{
-            jsonObject = JSONObject.fromObject(content);
-        } catch (Exception e){
-            return "invalid json format";
-        }
-
-        //添加字段
-        //jsonObject.accumulate("hello", "test");
-
-        //获取key值对应的数据
-        //String name = jsonObject.getString("hello");
-
-        try{
-            String code = jsonObject.getString("code");
-            ArrayList<Connect> data;
-            if (code.matches("^[0-9]*")) {
-                data = connectMapper.getDataByCode(code);
-            } else{
-                data = connectMapper.getDataByName(code);
-            }
-            if(data == null)
-                return "cannot find it";
-            JSONObject result = new JSONObject();
-            result.accumulate("code", data.get(0).getCode());
-            result.accumulate("name", data.get(0).getName());
-            return result;
-        }catch (Exception e){
-            return "Fail!";
-        }
+//        //将post过来的数据转换为json格式
+//        JSONObject jsonObject;
+//        try{
+//            jsonObject = JSONObject.fromObject(content);
+//        } catch (Exception e){
+//            return "invalid json format";
+//        }
+//
+//        //添加字段
+//        //jsonObject.accumulate("hello", "test");
+//
+//        //获取key值对应的数据
+//        //String name = jsonObject.getString("hello");
+//
+//        try{
+//            String code = jsonObject.getString("code");
+//            ArrayList<Connect> data;
+//            if (code.matches("^[0-9]*")) {
+//                data = connectMapper.getDataByCode(code);
+//            } else{
+//                data = connectMapper.getDataByName(code);
+//            }
+//            if(data == null)
+//                return "cannot find it";
+//            JSONObject result = new JSONObject();
+//            result.accumulate("code", data.get(0).getCode());
+//            result.accumulate("name", data.get(0).getName());
+//            return result;
+//        }catch (Exception e){
+//            return "Fail!";
+//        }
+        return "test!";
     }
 }
--- demo/src/main/java/edu/tongji/demo/Controller/StockController.java
+++ demo/src/main/java/edu/tongji/demo/Controller/StockController.java
@@ -1,13 +1,13 @@
 package edu.tongji.demo.Controller;
 
+import edu.tongji.demo.Mapping.ConnectMapper;
 import edu.tongji.demo.Mapping.IndustryMapper;
+import edu.tongji.demo.Model.Connect;
 import edu.tongji.demo.Model.Industry;
+import net.sf.json.JSONObject;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -18,8 +18,41 @@ public class StockController {
     @Autowired
     private IndustryMapper industryMapper;
 
+    @Autowired
+    private ConnectMapper connectMapper;
+
     @GetMapping("/GetAll")
     public ArrayList<Industry> GetAllStockInfo(){
         return industryMapper.getAllIndustryInfor();
+    }
+
+    @PostMapping("/GetOne")
+    public Object GetSpecificInfo(@RequestBody String content){
+        JSONObject jsonObject;
+        try{
+            jsonObject = JSONObject.fromObject(content);
+        } catch (Exception e){
+            return "invalid json format";
+        }
+        try{
+            String code = jsonObject.getString("code");
+            ArrayList<Connect> data;
+            if (code.matches("^[0-9]*")) {
+                data = connectMapper.getDataByCode(code);
+            } else{
+                data = connectMapper.getDataByName(code);
+            }
+            if(data == null)
+                return "cannot find it";
+
+            
+
+            JSONObject result = new JSONObject();
+            result.accumulate("code", data.get(0).getCode());
+            result.accumulate("name", data.get(0).getName());
+            return result;
+        }catch (Exception e){
+            return "Fail!";
+        }
     }
 }
