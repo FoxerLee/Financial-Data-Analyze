7102267dcf2b281c238f2cd2b8d273095ebe24c3
hujiaxin <470348052@qq.com>
2017-12-08 09:55:59 +0800

    add something

---
+++ demo/src/main/java/edu/tongji/demo/Controller/NewsController.java
@@ -0,0 +1,21 @@
+package edu.tongji.demo.Controller;
+
+import edu.tongji.demo.Mapping.NewsMapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/news")
+public class NewsController {
+
+    @Autowired
+    private NewsMapper newsMapper;
+
+    @GetMapping("/user")
+    public Object getNews(@Param(value = "code") String code){
+        return newsMapper.getNews(code);
+    }
+}
