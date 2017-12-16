c853104a8616892bc9ffbc63ec7549ad903a295e
hujiaxin <470348052@qq.com>
2017-12-16 23:32:44 +0800

    改动了ResearchMapper的部分

--- demo/src/main/java/edu/tongji/demo/Controller/ResearchController.java
+++ demo/src/main/java/edu/tongji/demo/Controller/ResearchController.java
@@ -7,6 +7,14 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 
 @RestController
@@ -22,6 +30,44 @@ public class ResearchController {
             return researchMapper.getBriefResearchData(code);
         }catch (Exception e){
             return "400";
+        }
+    }
+
+    @GetMapping("/personal")
+    public Object getResByPerson(HttpServletRequest request, HttpServletResponse response) throws IOException{
+//        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+//        HttpServletRequest request = attributes.getRequest();
+//
+//        HttpSession session = request.getSession(false);
+//        if(session == null){
+//            response.sendRedirect("http://localhost:8080/loginpage");
+//            return false;
+//        }
+//        else{
+//
+//        }
+//        try{
+//
+//        }catch (Exception e){
+//            System.out.println("error!!");
+//            return null;
+//        }
+        Cookie[] cookies = request.getCookies();
+        String name = "";
+        if(cookies == null){
+            response.sendRedirect("http://localhost:8080/loginpage");
+            return null;
+        }
+        else{
+            for (int i = 0; i < cookies.length; i++){
+                if(cookies[i].getName().equals("fnan")){
+                    name = cookies[i].getValue();
+                    System.out.println(name);
+                    return researchMapper.getPersonalStock(name);
+                }
+            }
+            response.sendRedirect("http://localhost:8080/loginpage");
+            return false;
         }
     }
 
--- demo/src/main/java/edu/tongji/demo/Verification.java
+++ demo/src/main/java/edu/tongji/demo/Verification.java
@@ -9,25 +9,24 @@ import javax.servlet.http.HttpSession;
 
 public class Verification {
     public static Boolean verify(){
-//        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-//        HttpServletRequest request = attributes.getRequest();
-//
-//        HttpSession session = request.getSession(false);
-//        if(session == null){
-//            return false;
-//        }
-//        else{
-//            Cookie[] cookies = request.getCookies();
-//            if(cookies == null)
-//                return false;
-//            else{
-//                for (int i = 0; i < cookies.length; i++){
-//                    if(cookies[i].getName().equals("fnan") && cookies[i].getValue().equals(session.getAttribute("name")))
-//                        return true;
-//                }
-//                return false;
-//            }
-//        }
-        return true;
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        HttpSession session = request.getSession(false);
+        if(session == null){
+            return false;
+        }
+        else{
+            Cookie[] cookies = request.getCookies();
+            if(cookies == null)
+                return false;
+            else{
+                for (int i = 0; i < cookies.length; i++){
+                    if(cookies[i].getName().equals("fnan") && cookies[i].getValue().equals(session.getAttribute("name")))
+                        return true;
+                }
+                return false;
+            }
+        }
     }
 }
