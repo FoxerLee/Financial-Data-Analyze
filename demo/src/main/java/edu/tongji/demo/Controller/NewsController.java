6a9e7c37377bcc41d91098b361bf44a6d2aa4711
DigitalSprite <1552635@tongji.edu.cn>
2017-12-19 00:03:16 +0800

    temp

--- demo/src/main/java/edu/tongji/demo/Controller/NewsController.java
+++ demo/src/main/java/edu/tongji/demo/Controller/NewsController.java
@@ -4,6 +4,7 @@ import edu.tongji.demo.Mapper.NewsMapper;
 import edu.tongji.demo.config.Verification;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
